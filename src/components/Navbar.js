e35b81990e6861e9fe416736f512ca0415f0a4c7
jayeshlambat <jayeshlambat02@gmail.com>
2024-07-07 21:47:21 +0530

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+
+
 function Navbar() {
   return (
     <div className='bg-white h-[5rem] rounded-lg my-4 flex flex-row items-center justify-center gap-6 ' >
-      <img src="/firebase.png" alt='err' />
+      <img src="../Images/firebase.png" alt='' />
       <h2 className='font-medium text-xl '>Firebase Contact App</h2>
 
     </div>
--- src/components/NotFoundContact.js
+++ src/components/NotFoundContact.js
@@ -1,11 +1,10 @@
 import React from 'react'
-
 function NotFoundContact() {
     return (
         <div>
             <div className="flex h-[80vh] items-center justify-center gap-4">
                 <div>
-                    <img src="/contact.png" alt='' />
+                    <img src="../Images/contact.png" alt='' />
                 </div>
                 <h3 className="text-2xl font-semibold text-white"> Contact Not Found</h3>
             </div>
